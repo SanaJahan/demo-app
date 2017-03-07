f1ee101235d37f1cc560065e06815ba7ecd57b4f
Sana Jahan <sanadgrt@gmail.com>
2017-03-08 02:20:37 +0530

    added header

--- UI/main.js
+++ UI/main.js
@@ -71,7 +71,6 @@ var register = document.getElementById('register_btn');
                 <meta name = "viewport" content = "width = device-width initial-scale=1" />
                   <title></title>
                         <body>
-                            <div id="list">
                             <ul>
                             <li>
                     <a href="/patient/${listData[i].userid}">${listData[i].firstname} ${listData[i].lastname}</a>
@@ -79,7 +78,7 @@ var register = document.getElementById('register_btn');
 
 
                 }
-                content += "</ul></div><br><br></body>";
+                content += "</ul><br><br></body>";
                 list.innerHTML = content;
             }
             else {
