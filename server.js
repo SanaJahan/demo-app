c14c1c46affeb0f41aaaed2213bb025b680a3d85
Sana Jahan <sanadgrt@gmail.com>
2017-03-07 16:48:50 +0530

    changed ui

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ function createNewFormTemplate(){
                         form.submit();
                       }
                     }
-                  }
+                    });
                   });
             </script>
             </head>
