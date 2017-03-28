7e84aaf791ba9a957067bbd3fe553a873a573215
Sana Jahan <sanadgrt@gmail.com>
2017-03-28 05:40:25 +0530

    added insert read update delete functions

--- UI/main.js
+++ UI/main.js
@@ -123,7 +123,170 @@ var register = document.getElementById('register_btn');
     request.open('GET', '/home', true);
     request.send(null);
 }
+// Make request to update the information 
+var deleted = document.getElementById("delete");
 
-// List pf patients loaded on home page load
+ if (deleted != undefined) {
+ 
+    deleted.onclick = function () {
+       if(confirm('Delete record?')){
+        // Create a request object
+         var id = document.getElementById("delete").value;
+         console.log(id);
+        var request = new XMLHttpRequest();
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              if (request.status === 200) {
+                    window.location.href = "/";
+
+                }
+                // If page fails to load
+              else if(request.status === 403){
+                  alert('Could not delete the user');
+          }
+          else if(request.status === 503){
+                  alert('Could not delete the user');
+                }
+          }
+           
+        };
+        // Make the request
+        request.open('DELETE', window.location.protocol+'//'+window.location.host+'/delete/'+id, true);
+        request.send(null);
+
+   }
+ }
+}
+// Update form
+var unUpdated = document.getElementById("updated");
+if(unUpdated != undefined){
+  unUpdated.onclick = function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              if (request.status === 200) {
+                    var info = JSON.parse(this.responseText);
+                     window.location.href = "/update/"+id;
+                     var id=document.getElementById("update").value;
+                     document.getElementById('firstname').value = '${list.firstname}';
+                     document.getElementById('lastname').value= '${list.lastname}';
+                     document.getElementById('age').value= '${list.age}';
+                     document.getElementById('dob').value= '${list.dob}';
+                     document.getElementById('gender').value= '${list.gender}';
+                     document.getElementById('phone').value= '${list.phone}';
+                     document.getElementById('random_text').value= '${list.randomtext}';
+
+                }
+                // If page fails to load
+              else if(request.status === 403){
+                  alert('Could not update the user');
+          }
+          else if(request.status === 503){
+                  alert('Could not update the user');
+                }
+          }
+        };
+
+        request.open('GET', window.location.protocol+'//'+window.location.host+'/update/'+id, true);
+        request.send(null);
+
+  };
+}
+
+
+
+// Updating and POsting in the form
+var updated = document.getElementById("update");
+if(updated != undefined){
+  updated.onclick = function(){
+    var id = document.getElementById("update").value;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+               if (request.status === 200) {
+                  document.location.href = "/patient/"+id;
+                }
+                // If page fails to load
+              else if(request.status === 403){
+                  alert('Could not update the user');
+                  update.value = 'Update';
+          }
+          else if(request.status === 503){
+                  alert('Could not update the user');
+                  update.value = 'Update';
+                }
+          } 
+        };
+        // Make the request
+        var firstname = document.getElementById('firstname').value;
+        var lastname =  document.getElementById('lastname').value;
+        var age =  document.getElementById('age').value;
+        var dob =  document.getElementById('DOB').value;
+        var gender = document.getElementById('gender').value;
+        var phone = document.getElementById('phone').value;
+        var randomtext = document.getElementById('random_text').value;
+           //Validation for firstname
+           var regex = /^[a-zA-Z]*$/;
+              if(firstname === '' || !(firstname.match(regex)) ){
+                   document.getElementById('firstname').placeholder = "Enter valid first name ";
+                   document.registration.firstname.focus();
+                   return false;
+           }
+           //check spaces between names
+           if(firstname.match(' ')){
+                   alert('No Spaces');
+                   document.registration.firstname.focus();
+                   return false;
+                }
+                if(lastname.match(' ')){
+                   alert('No Spaces');
+                   document.registration.lastname.focus();
+                   return false;
+                }
+          //Validation for lastname
+            if(lastname === '' || !(lastname.match(regex)) ){
+                   document.getElementById('lastname').placeholder = "Enter valid last name";
+                   document.registration.lastname.focus();
+                   return false;
+           }
+           //Validation for dob
+            if(dob === ''){
+                   document.getElementById('DOB').placeholder = "Enter birthday ";
+                   document.registration.DOB.focus();
+                   return false;
+           }
+         if(age <0 || age > 100 || age === '')
+                 {
+                   alert("Enter valid birthday ");
+                   document.registration.DOB.focus();
+                   return false;
+                 }
+
+  // Validation for gender
+                  if(gender === "select"  )
+         {
+           alert("Enter valid gender ");
+           document.registration.gender.focus();
+           return false;
+         }
+
+//Validation for phone number
+        if(phone.toString().length != 10)
+         {
+           document.getElementById('phone').innerHTML = "Enter valid number ";
+           document.registration.phone.focus();
+           return false;
+         }
+
+        //request.open('POST','/patient/update/'+id, true);
+        request.open('POST', window.location.protocol+'//'+window.location.host+'/patient/update/'+id, true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({firstname: firstname, lastname: lastname,dob: dob,age: age,gender: gender, phone: phone, randomtext: randomtext})); 
+    };
+   }
+
+
+// List of patients loaded on home page load
 loadList();
 
--- server.js
+++ server.js
@@ -30,7 +30,7 @@ function createTemplate(listdata){
     var gender = listdata.gender;
     var dob = listdata.dob;
     var phone=listdata.phone;
-    var randomtext = listdata.randomtext;
+    var id = listdata.userid;
      
         var htmlTemplate =  `<html>
             <head>
@@ -42,19 +42,19 @@ function createTemplate(listdata){
                  href="https://fonts.googleapis.com/css?family=Baloo Tamma|David Libre|Open Sans Condensed|Ubuntu|Pavanam|Pacifico">
             </head>
             <body>
-            <div id="app-heading"><header><h1 align = "left">Demo-App V1</h1></header></div><br/><br/>
-                <div style="font-family: Pacifico;font-size: 1.5em">
+            <div id="app-heading"><header><h1 align = "left">CRUD APPLICATION</h1></header></div><br/><br/>
+                <div style="font-family: Ubuntu;font-size: 1.5em">
                 <a href = "/">Click here to go back to homepage</a>
                    <center><p>
                    <h2><u> Details : </u></h2>
-                    Name : ${firstname} ${lastname}  <br>
-                    Age  : ${age} </br>
-                    DOB  : ${dob} </br>
-                    Gender: ${gender} <br/>
-                    Phone : ${phone} <br/>
-                    Query : ${randomtext} <br/>
+                    <label id="firstname">Name :</label> ${firstname} <label id = "lastname">${lastname}</label>  <br>
+                    <label id="age">Age  :</label> ${age} </br>
+                    <label id="dob">DOB  :</label> ${dob} </br>
+                    <label id="gender">Gender:</label> ${gender} <br/>
+                    <label id="phone">Phone :</label> ${phone} <br/>
                    </p></center>
                 </div>
+                <p align = "center"><button id="updated" value = ${id} ><a href="/update/${id}">Update</a></button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button id="delete" value = "${id}" >Delete</button></p>
            <script type="text/javascript" src="/UI/main.js"></script>
            </body>      
         </html> ` ;
@@ -104,7 +104,7 @@ function createNewFormTemplate(){
                     </script>
             </head>
             <body>
-            <div id="app-heading"><header><h1 align = "left">Demo-App V1</h1></header></div><br/><br/>
+            <div id="app-heading"><header><h1 align = "left">CRUD APPLICATION</h1></header></div><br/><br/>
                 <div class=container-articles>
                     <a href="/">Home</a>
                 </div>
@@ -143,12 +143,100 @@ function createNewFormTemplate(){
         </html> ` ;
         return htmlNewFormTemplate;
 }
+//Update template
+function createUpdateTemplate(listid){
+  var id = listid.userid;
+  var firstname = listid.firstname;
+  var lastname = listid.lastname;
+  var age = listid.age;
+  var gender = listid.gender;
+  var dob = listid.dob;
+  var phone=listid.phone;
+  var randomtext = listid.randomtext;
 
+        var htmlNewFormTemplate =  `<html>
+            <head>
+                <title>Please enter following details</title>
+                <meta name = "viewport" content = "width = device-width initial-scale=1" />
+                <link href="/UI/style.css" rel="stylesheet" />
+                     <style>
+                        input[type=text]:focus {
+                            background-color: lightblue;
+                        }
+                        input[type=number]:focus {
+                            background-color: lightblue;
+                        }
+                      </style>  
+                   <link rel="stylesheet" href="https://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+                  <!-- Load jQuery JS -->
+                  <script src="https://code.jquery.com/jquery-1.9.1.js"></script>
+                  <!-- Load jQuery UI Main JS  -->
+                  <script src="https://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+      
+                    <script>
+                          $(function() {
+                          
+                           $('#DOB').datepicker({
+                          onSelect: function(value, ui) {
+                              var today = new Date(),
+                                  dob = new Date(value),
+                                  age = today.getFullYear() - ui.selectedYear;
+
+                              $('#age').val(age);
+                          },
+                          maxDate: '+0d',
+                          yearRange: '1917:'+(new Date).getFullYear(),
+                          changeMonth: true,
+                          changeYear: true
+                      });
+
+                    });
+                    </script>
+            </head>
+            <body>
+            <div id="app-heading"><header><h1 align = "left">CRUD APPLICATION</h1></header></div><br/><br/>
+                <div class=container-articles>
+                    <a href="/">Home</a>
+                </div>
+                <hr />
+                <h2>Register here</h2>
+                <div id="register_area">
+                <form name="registration" id="registration">
+                <p class="contact"><label for="firstname">First Name</label></p>
+                <input id="firstname" name="firstname" placeholder="First name" required="" tabindex="1" type="text" value=${firstname} pattern='[A-Za-z\\s]*'>
+                <p class="contact"><label for="lastname">Last Name</label></p>
+                <input id="lastname" name="lastname" placeholder="Last name" required="" tabindex="1" type="text" value=${lastname} pattern='[A-Za-z\\s]*'>
+                <p class="contact"><label for="dob">Your birthday</label></p>
+                <input id="DOB" name="DOB" placeholder="Your birthdate" required="" type="text" value=${dob}>
+                <p class="contact"><label for="age">Your age</label></p>
+                <input id="age" name="age" placeholder="Your age" required="" type="number" readonly ="readonly">
+                <p class="contact"><label for="gender">Your gender</label></p>
+                <select class="select-style gender" name="gender" id="gender" >
+                <option value="select">I am..</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="others">Other</option>
+                        </select><br><br>
+                <p class="contact"><label for="phone">Mobile phone</label></p>
+                <input id="phone" name="phone" placeholder="phone number" type="number" value=${phone}>
+                <p class="contact"><label for="text_area" id="random_text">Any queries</label></p>
+                <textarea id="random_text" placeholder="Type your queries here" rows="5" cols="60" name = "random_text">${randomtext}</textarea><br>
+                <center><button class="button" name="submit" id="update" tabindex="5" type="submit" value = ${id}>Update</button></center>  
+                </form>                            
+                <br>
+                </div>
+                <br><hr>
+               <script type="text/javascript" src="/UI/main.js">
+        </script>
+            </body>
+        </html> ` ;
+        return htmlNewFormTemplate;
+}
         
 // Creating the database pool
 //var env = new Client(process.env.DATABASE_URL);
 var env = ("postgres://bhvvtuxplftdwy:a01f6772dcf0f73f9625dc05c03c6e0323cc57613a2c653016948360569f3c91@ec2-54-225-67-3.compute-1.amazonaws.com:5432/d1r25ado6kk8t9?ssl=true");
-var pool = new Pool.Client(env); 
+var pool = new Pool.Client(env || config); 
 pool.connect();
 //INSERTING THE USERNAME and DETAILS FOR REGISTRATION
 
@@ -161,7 +249,8 @@ app.post('/create-user',function(req,res){
   var gender = req.body.gender;
   var phone = req.body.phone;
   var randomtext=req.body.randomtext;
-  pool.query('INSERT INTO "patient_info" (firstname,lastname,dob,age,gender,phone,randomtext) VALUES ($1,$2,$3,$4,$5,$6,$7)',[firstname,lastname,dob,age,gender,phone,randomtext],function(err,result){
+  pool.query('INSERT INTO "patient_info" (firstname,lastname,dob,age,gender,phone,randomtext) VALUES ($1,$2,$3,$4,$5,$6,$7)',
+    [firstname,lastname,dob,age,gender,phone,randomtext],function(err,result){
      if(err){
            res.status(500).send(err.toString());
        }
@@ -189,6 +278,7 @@ app.get('/home',function(req,res){
   });
 //API to get separate patient details
 app.get('/patient/:userid',function(req,res){
+
    //make a select request and return result set
    pool.query("SELECT * FROM patient_info WHERE userid= $1 ",[req.params.userid],function(err,result){
        if(err){
@@ -207,11 +297,72 @@ app.get('/patient/:userid',function(req,res){
        
 
 });
+// USER INFORMATION UPDATE
+app.post('/patient/update/:userid',function(req,res){
+  var id =  req.params.userid;
+  var firstname = req.body.firstname;
+  var lastname = req.body.lastname;
+  var age = req.body.age;
+  var dob = req.body.dob;
+  var gender = req.body.gender;
+  var phone = req.body.phone;
+  var randomtext = req.body.randomtext;
+pool.query("UPDATE patient_info SET firstname =($1), lastname=($2), dob=($3), age=($4), gender=($5), phone=($6), randomtext=($7) WHERE userid=($8)",
+    [firstname, lastname, dob, age, gender, phone, randomtext,id],function(err,result){
+        if(err){
+          res.status(500).send(err.toString());
+          console.log("error:",err);
+        }
+        else {
+
+          res.send('User information updated succesfully '+ firstname);
+          console.log(result.rows[0]);
+        }
+
+
+    });
+
+
+
+});
+
+//  delete record from the list
+app.delete('/delete/:userid',function(req,res){
+  pool.query("DELETE FROM patient_info WHERE userid = ($1)",[req.params.userid],function(err,result){
+    if(err){
+      res.status(500).send(err.toString());
+      console.log("error:",err);
+        }
+         else {
+
+          res.send('User information DELETED succesfully ');
+        }
+
+  });
+});
 
 //API for going to the registration form page
 app.get('/newUser',function(req,res){
     res.send(createNewFormTemplate());
        });
+//API for the update form
+app.get('/update/:userid',function(req,res){
+  var id = req.params.userid;
+  pool.query("SELECT * FROM patient_info WHERE userid= $1 ",[req.params.userid], function (err,result){
+    if(err){
+        res.status(500).send(err.toString());
+    }
+     else{
+        if(result.rows.length===0){
+          res.status(404).send('patient not found');
+        }
+    else {
+        information=(result.rows[0]);
+        res.send(createUpdateTemplate(information));
+        } 
+        } 
+   });
+});
 
         //Including all the files
 app.get('/', function (req, res) {
